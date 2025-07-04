b02df42404de45d17a1b18786054284edc218c05
HKMUSIC <himanshu4hk9@gmail.com>
2025-07-04 18:17:18 +0530

    Update convert.py

--- TEAMZYRO/modules/convert.py
+++ TEAMZYRO/modules/convert.py
@@ -6,8 +6,8 @@ from pyrogram.types import Message
 # MongoDB Setup (Your DB)
 MONGO_URL = "mongodb+srv://Gojowaifu:waifu123@gojowaifu.royysxq.mongodb.net/?retryWrites=true&w=majority&appName=Gojowaifu"
 mongo = MongoClient(MONGO_URL)
-db = mongo["waifu_bot"]         # You can rename if needed
-users = db["users"]             # Make sure this is your correct collection name
+db = mongo["waifu_bot"]
+users = db["users"]
 
 TOKEN_RATE = 100  # 100 coins = 1 token
 
@@ -20,11 +20,11 @@ async def convert_coins(client, message: Message):
         return await message.reply("❌ Usage: `/convert 100`", quote=True)
 
     coins_to_convert = int(args[1])
-    user_data = users.find_one({"user_id": user_id})
+    user_data = users.find_one({"_id": user_id})
 
     if not user_data:
-        users.insert_one({"user_id": user_id, "coins": 0, "tokens": 0})
-        user_data = users.find_one({"user_id": user_id})
+        users.insert_one({"_id": user_id, "coins": 0, "tokens": 0})
+        user_data = users.find_one({"_id": user_id})
 
     current_coins = user_data.get("coins", 0)
     current_tokens = user_data.get("tokens", 0)
@@ -40,7 +40,7 @@ async def convert_coins(client, message: Message):
     new_tokens = current_tokens + tokens_earned
 
     users.update_one(
-        {"user_id": user_id},
+        {"_id": user_id},
         {"$set": {"coins": coins_after, "tokens": new_tokens}}
     )
 
