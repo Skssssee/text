5d40b65e3660f87e59265df77d09ec9684f3d83b
HKMUSIC <himanshu4hk9@gmail.com>
2025-07-11 17:49:29 +0530

    Update start.py

--- TEAMZYRO/modules/start.py
+++ TEAMZYRO/modules/start.py
@@ -41,7 +41,7 @@ async def generate_start_message(client, message):
         [InlineKeyboardButton("❍sᴜᴘᴘᴏʀᴛ❍", url="https://t.me/GOJO_NOBITA_II"), 
          InlineKeyboardButton("❍ᴄʜᴀɴɴᴇʟ❍", url="https://t.me/GOJO_SUPPORT_GROUP_II")],
         [InlineKeyboardButton("⋆ʜᴇʟᴘ⋆", callback_data="open_help")],
-        [InlineKeyboardButton("✦ʟᴏʀᴅ✦", url="http://t.me/II_YOUR_GOJO_ll")]
+        [InlineKeyboardButton("✦ʟᴏʀᴅ✦", url="http://t.me/HeartStealer_X")]
     ]
     
     return caption, buttons
