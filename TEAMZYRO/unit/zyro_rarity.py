7e7af7db13ca99ce13f127ee534863d743b3d520
Skssssee <91522489+Skssssee@users.noreply.github.com>
2025-09-10 16:01:58 +0530

    Update zyro_rarity.py

--- TEAMZYRO/unit/zyro_rarity.py
+++ TEAMZYRO/unit/zyro_rarity.py
@@ -1,57 +1,58 @@
 rarity_map = {
-    1: "⚪️ Low",
-    2: "🟠 Medium",
-    3: "🔴 High",
-    4: "🎩 Special Edition",
-    5: "🪽 Elite Edition",
-    6: "🪐 Exclusive",
-    7: "💞 Valentine",
-    8: "🎃 Halloween",
-    9: "❄️ Winter",
-    10: "🏖 Summer",
-    11: "🎗 Royal",
-    12: "💸 Luxury Edition",
-    13: "🍃 echhi",
-    14: "🌧️ Rainy Edition",
-    15: "🎍 Festival"
+ 1: "🟣 Common",              
+ 2: "🟠 Rare",          
+ 3: "🟡 Legendary",   
+ 4: "💮 Mythic",   
+ 5: "⚜️ Devine",     
+ 6: "⚡️ Crossverse",
+ 7: "✨ Cataphract" ,
+ 8: "🪞 Supreme",
+    #9: "❄️ Winter",
+    #10: "🏖 Summer",
+    #11: "🎗 Royal",
+    #12: "💸 Luxury Edition",
+    #13: "🍃 echhi",
+    #14: "🌧️ Rainy Edition",
+   # 15: "🎍 Festival",
+    
 }
 
 RARITY_NAMES = [
-    "⚪️ Low",
-    "🟠 Medium",
-    "🔴 High",
-    "🎩 Special Edition",
-    "🪽 Elite Edition",
-    "🪐 Exclusive",
-    "💞 Valentine",
-    "🎃 Halloween",
-    "❄️ Winter",
-    "🏖 Summer",
-    "🎗 Royal",
-    "💸 Luxury Edition",
-    "🍃 echhi",
-    "🌧️ Rainy Edition",
-    "🎍 Festival"
+  "🟣 Common",              
+  "🟠 Rare",        
+  "🟡 Legendary",   
+  "💮 Mythic",   
+  "⚜️ Devine",     
+  "⚡️ Crossverse",
+  "✨ Cataphract",
+  "🪞 Supreme",
+  #  "❄️ Winter",
+  #$  "🏖 Summer",
+   # "🎗 Royal",
+  #  "💸 Luxury Edition",
+ # "🍃 echhi",
+  #  "🌧️ Rainy Edition",
+  #  "🎍 Festival",
+   
+    
 ]
 
 rarity_map2 = {
-    "⚪️ Low": "⚪️",
-    "🟠 Medium": "🟠",
-    "🔴 High": "🔴",
-    "🎩 Special Edition": "🎩",
-    "🪽 Elite Edition": "🪽",
-    "🪐 Exclusive": "🪐",
-    "💞 Valentine": "💞",
-    "🎃 Halloween": "🎃",
-    "❄️ Winter": "❄️",
-    "🏖 Summer": "🏖",
-    "🎗 Royal": "🎗",
-    "💸 Luxury Edition": "💸",
-    "🍃 echhi": "🍃",
-    "🌧️ Rainy Edition": "🌧️",
-    "🎍 Festival": "🎍"
+
+  "🟣 Common" :"🟣",              
+  "🟠 Rare" : "🟠",          
+  "🟡 Legendary": "🟡",   
+  "💮 Mythic": "💮",   
+  "⚜️ Devine": "⚜️",     
+  "⚡️ Crossverse": "⚡️",
+  "✨ Cataphract": "✨",
+  "🪞 Supreme": "🪞",
+    #"❄️ Winter": "❄️",
+   # "🏖 Summer": "🏖",
+    #"🎗 Royal": "🎗",
+   # "💸 Luxury Edition": "💸",
+    #"🍃 echhi": "🍃",
+   # "🌧️ Rainy Edition": "🌧️",
+ #   "🎍 Festival": "🎍",
+    
 }
-
-
-
-
